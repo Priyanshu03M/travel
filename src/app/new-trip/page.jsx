a7532daa0711f708cebd6a783444e2a9f2042633
Priyanshu03M <97392355+Priyanshu03M@users.noreply.github.com>
2024-07-20 16:24:08 +0530

    Secondpage

--- src/app/new-trip/page.jsx
+++ src/app/new-trip/page.jsx
@@ -1,9 +1,20 @@
+import Blog from "../components/blog";
 import Navbar from "../components/navbar";
 
-function Hotels(){
+function Plan(){
     return (
-        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <h3>Plan a new Trip</h3>
+                <form action="https://formsubmit.co/Mohitknkush@gmail.com" method="POST">
+                    <input name="Destination" type="text" placeholder="Destination"/>
+                    <input name="Date" type="date"/>
+                    <button>Send</button>
+                </form>
+            </div>
+            <Navbar/>
+        </div>
     );
 }
 
-export default Hotels;
+export default Plan;
